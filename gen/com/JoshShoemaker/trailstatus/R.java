b933a06b055cd9411dfb34ae3f669d40db18d23d
Joshua Shoemaker <shoejosh@gmail.com>
2013-02-11 19:16:07 -0600

    Updated refresh button to use icon instead of text

--- gen/com/JoshShoemaker/trailstatus/R.java
+++ gen/com/JoshShoemaker/trailstatus/R.java
@@ -22,9 +22,11 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
         public static final int myshape=0x7f020001;
-        public static final int preview=0x7f020002;
-        public static final int widget_frame=0x7f020003;
-        public static final int widget_frame_old=0x7f020004;
+        public static final int navigation_refresh=0x7f020002;
+        public static final int navigation_refresh_white=0x7f020003;
+        public static final int preview=0x7f020004;
+        public static final int widget_frame=0x7f020005;
+        public static final int widget_frame_old=0x7f020006;
     }
     public static final class id {
         public static final int btnRefresh=0x7f080006;
